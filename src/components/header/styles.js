af0baca5122c86e42a62d19e9c631f49c9e688c6
Stephyarr <stephaniearratia21@gmail.com>
2023-01-30 18:21:08 -0300

    firts commit

---
+++ src/components/header/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#7E4D93",
+    paddingTop: 50,
+    height: 120,
+  },
+  title: {
+    fontSize: 18,
+    textAlign: "center",
+  },
+});
